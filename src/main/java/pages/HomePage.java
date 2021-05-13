33e393891e3af09d4162379d8f1185235eeb9ec0
Hermine M <militonyan.hermine@gmail.com>
2021-05-13 16:57:13 +0400

    initial test

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base.POMBase;
+
+public class HomePage extends POMBase {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
